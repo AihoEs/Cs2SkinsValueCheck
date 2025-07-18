b0f61163d30ef3011fc69bf38f14552ab94348c2
AihoEs <efoefim@gmail.com>
2025-07-18 23:16:10 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def send_telegram_message(message: str):
    
     url = (
         f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
-        f"?chat_id={TELEGRAM_CHAT_ID}&text={urllib.parse.quote('ЗАПУСК')}"
+        f"?chat_id={TELEGRAM_CHAT_ID}&text={urllib.parse.quote(message)}"
     )
     try:
         requests.get(url)
